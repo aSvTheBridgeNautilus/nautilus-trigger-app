711e0e8612d1b0c08f892f288174f799a4768810
aSvTheBridgeNautilus <80336933+aSvTheBridgeNautilus@users.noreply.github.com>
2021-05-15 21:01:21 -0500

    NCB Cash-Out failure test enviropment ready!

--- src/main/java/com/nautilus/trigger/app/NautilusTriggerApplication.java
+++ src/main/java/com/nautilus/trigger/app/NautilusTriggerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NautilusProccesorTriggerAppApplication {
+public class NautilusTriggerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NautilusProccesorTriggerAppApplication.class, args);
+		SpringApplication.run(NautilusTriggerApplication.class, args);
 	}
 
 }
--- src/main/java/com/nautilus/trigger/app/application/controllers/NCBCloudEventProducerController.java
+++ src/main/java/com/nautilus/trigger/app/application/controllers/NCBCloudEventProducerController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,21 +13,26 @@ import com.nautilus.trigger.app.application.dto.record.NCBTransferCashOutFailure
 import com.nautilus.trigger.app.service.CloudEventProvider;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/trigger-app")
 @RequiredArgsConstructor
+@Slf4j
 public class NCBCloudEventProducerController {
 	
 	private final CloudEventProvider eventProvider;
 	
+	@PostMapping(value = "/publish")
 	public ResponseEntity<Object> produceNCBTransferCashOutFailure(
 			@RequestBody NCBTransferCashOutFailureRecord payloadRequest){
+		log.info("[NCBCloudEventProducerController:produceNCBTransferCashOutFailure] NCB Transfer Cash-Out Failure started");
 		
 		eventProvider.publish(payloadRequest);
 		
 		Map<String, Object> responseBody = new HashMap<>();
 		responseBody.put("NCB Transfer Cash-Out Failure", "Message Sent!");
+		responseBody.put("Event Type", payloadRequest.getEventType());
 		responseBody.put("Payload", payloadRequest);
 		
 		return ResponseEntity.ok(responseBody);
