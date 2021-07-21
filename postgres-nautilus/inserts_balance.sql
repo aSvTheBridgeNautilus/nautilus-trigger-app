6f646b3e9987c6d1aea33373d8b6c434d99b9672
asv <asv@localhost>
2021-07-21 11:54:58 -0500

    updated queries

---
+++ postgres-nautilus/inserts_balance.sql
@@ -0,0 +1,57 @@
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(1,26, 0, 0, 'JMD', 'Init Balance', 1001, 2, 1, 'emanuel.Herrera@nautilus.team', 'emanuel.Herrera@nautilus.team', '2021-01-01T00:00:00Z','emanuel.Herrera@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(2,27, 0, 0, 'JMD', 'Init Balance', 1002, 2, 1, 'augusto.silveira@nautilus.team', 'augusto.silveira@nautilus.team', '2021-01-01T00:00:00Z','augusto.silveira@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(3,28, 0, 0, 'JMD', 'Init Balance', 1003, 2, 1, 'evandro.assis@nautilus.team','evandro.assis@nautilus.team', '2021-01-01T00:00:00Z','evandro.assis@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(4,29, 0, 0, 'JMD', 'Init Balance', 1004, 2, 1, 'miguel.godoy@nautilus.team', 'miguel.godoy@nautilus.team', '2021-01-01T00:00:00Z','miguel.godoy@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(5,30, 0, 0, 'JMD', 'Init Balance', 1005, 2, 1, 'simon.aguilera@nautilus.team', 'simon.aguilera@nautilus.team', '2021-01-01T00:00:00Z','simon.aguilera@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(6,31, 0, 0, 'JMD', 'Init Balance', 1006, 2, 1, 'edikson.garcia@nautilus.team', 'edikson.garcia@nautilus.team', '2021-01-01T00:00:00Z','edikson.garcia@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(7,32, 0, 0, 'JMD', 'Init Balance', 1007, 2, 1, 'carlos.marriaga@nautilus.team', 'carlos.marriaga@nautilus.team', '2021-01-01T00:00:00Z','carlos.marriaga@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(8,33, 0, 0, 'JMD', 'Init Balance', 1008, 2, 1, 'gonzalo.bordoy@nautilus.team', 'gonzalo.bordoy@nautilus.team', '2021-01-01T00:00:00Z','gonzalo.bordoy@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(9,34, 0, 0, 'JMD', 'Init Balance', 1009, 2, 1, 'andres.arias@nautilus.team', 'andres.arias@nautilus.team', '2021-01-01T00:00:00Z','andres.arias@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(10,35, 0, 0, 'JMD', 'Init Balance', 1010, 2, 1, 'deyvid.ferrer@nautilus.team', 'deyvid.ferrer@nautilus.team', '2021-01-01T00:00:00Z','deyvid.ferrer@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(11,36, 0, 0, 'JMD', 'Init Balance', 1011, 2, 1, 'victor.camacho@nautilus.team', 'victor.camacho@nautilus.team', '2021-01-01T00:00:00Z','victor.camacho@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(12,37, 0, 0, 'JMD', 'Init Balance', 1012, 2, 1, 'Enrique.Pinto@nautilus.team', 'Enrique.Pinto@nautilus.team', '2021-01-01T00:00:00Z','Enrique.Pinto@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(13,38, 0, 0, 'JMD', 'Init Balance', 1013, 2, 1, 'jorge.gomez@nautilus.team', 'jorge.gomez@nautilus.team', '2021-01-01T00:00:00Z','jorge.gomez@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(14,39, 0, 0, 'JMD', 'Init Balance', 1014, 2, 1, 'auth0|606e0ed9e1b94d0076ad4dc4', 'auth0|606e0ed9e1b94d0076ad4dc4', '2021-01-01T00:00:00Z','auth0|606e0ed9e1b94d0076ad4dc4');
+
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(15,26, 1000, 1000, 'JMD', 'Cash Deposit', 2001, 1, 2, 'emanuel.Herrera@nautilus.team', 'emanuel.Herrera@nautilus.team', Now(),'emanuel.Herrera@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(16,27, 1000, 1000, 'JMD', 'Cash Deposit', 2002, 1, 2, 'augusto.silveira@nautilus.team', 'augusto.silveira@nautilus.team', Now(),'augusto.silveira@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(17,28, 1000, 1000, 'JMD', 'Cash Deposit', 2003, 1, 2, 'evandro.assis@nautilus.team','evandro.assis@nautilus.team', Now(),'evandro.assis@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(18,29, 1000, 1000, 'JMD', 'Cash Deposit', 2004, 1, 2, 'miguel.godoy@nautilus.team', 'miguel.godoy@nautilus.team', Now(),'miguel.godoy@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(19,30, 1000, 1000, 'JMD', 'Cash Deposit', 2005, 1, 2, 'simon.aguilera@nautilus.team', 'simon.aguilera@nautilus.team', Now(),'simon.aguilera@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(20,31, 1000, 1000, 'JMD', 'Cash Deposit', 2006, 1, 2, 'edikson.garcia@nautilus.team', 'edikson.garcia@nautilus.team', Now(),'edikson.garcia@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(21,32, 1000, 1000, 'JMD', 'Cash Deposit', 2007, 1, 2, 'carlos.marriaga@nautilus.team', 'carlos.marriaga@nautilus.team', Now(),'carlos.marriaga@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(22,33, 1000, 1000, 'JMD', 'Cash Deposit', 2008, 1, 2, 'gonzalo.bordoy@nautilus.team', 'gonzalo.bordoy@nautilus.team', Now(),'gonzalo.bordoy@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(23,34, 1000, 1000, 'JMD', 'Cash Deposit', 1009, 1, 2, 'andres.arias@nautilus.team', 'andres.arias@nautilus.team', Now(),'andres.arias@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(24,35, 1000, 1000, 'JMD', 'Cash Deposit', 2010, 1, 2, 'deyvid.ferrer@nautilus.team', 'deyvid.ferrer@nautilus.team', Now(),'deyvid.ferrer@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(25,36, 1000, 1000, 'JMD', 'Cash Deposit', 2011, 1, 2, 'victor.camacho@nautilus.team', 'victor.camacho@nautilus.team', Now(),'victor.camacho@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(26,37, 1000, 1000, 'JMD', 'Cash Deposit', 2012, 1, 2, 'Enrique.Pinto@nautilus.team', 'Enrique.Pinto@nautilus.team', Now(),'Enrique.Pinto@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(27,38, 1000, 1000, 'JMD', 'Cash Deposit', 2013, 1, 2, 'jorge.gomez@nautilus.team', 'jorge.gomez@nautilus.team', Now(),'jorge.gomez@nautilus.team');
+INSERT INTO public.balance(id,account_id, amount, balance, currency, comments, movement_id, operation_type, reference_number, source_user_id, target_user_id, timestamp_utc, user_id) VALUES
+(28,39, 1000, 1000, 'JMD', 'Cash Deposit', 2014, 1, 2, 'auth0|606e0ed9e1b94d0076ad4dc4', 'auth0|606e0ed9e1b94d0076ad4dc4', Now(),'auth0|606e0ed9e1b94d0076ad4dc4');
